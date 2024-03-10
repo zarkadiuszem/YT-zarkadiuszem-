5159f78a81006baf30403ac05d25d736d0fa0a1c
zarkadiuszem <162899993+zarkadiuszem@users.noreply.github.com>
2024-03-10 16:10:18 +0100

    SQL v_1
    Skrypt do filmu SQL 1. Tworzenie jednej tabeli z 10 rekordami.

---
+++ biblioteka1.sql
@@ -0,0 +1,40 @@
+DROP DATABASE IF EXISTS Library;
+CREATE DATABASE IF NOT EXISTS Library; 
+USE Library;
+
+CREATE TABLE books (
+    booksID INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(32) NOT NULL,
+    number_of_pages SMALLINT NOT NULL,
+    publishing_house VARCHAR(32) NOT NULL,
+    PRIMARY KEY (booksID)
+);
+
+INSERT INTO `books` (
+title,
+number_of_pages,
+publishing_house
+)VALUES 
+("Labirynt Marzeń", 400, "Wydawnictwo Szept"),
+("Cienie Pamięci", 280, "Księgarnia Zmysłów"),
+("Tajemnicza Sonata", 350, "Alchemiczne Wydawnictwo"),
+("Obłok Nadziei", 420, "Nimbus Books"),
+("Wąż Ognistego Lata", 300, "Magia Literatury"),
+("Strzępy Czasu", 380, "Wydawnictwo Przeznaczenie"),
+("Zaklęte Wzgórze", 250, "Gryfonia Press"),
+("Mroczny Portal", 320, "Książki Złudzeń"),
+("Szeptem Gwiazd", 360, "Astralne Wydawnictwo"),
+("Klucz do Nieskończoności", 400, "Portal Fantazji Books");
+
+
+SELECT 
+    *
+FROM
+    books;
+    
+SELECT 
+    title, number_of_pages
+FROM
+    books
+WHERE
+    number_of_pages >= 400;
